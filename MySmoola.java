43585b747e709363b7220094dec15ba475475cd5
amir <iamirranjbar@gmail.com>
2018-12-16 00:44:44 +0330

    MySmoola.java is added and fixed to run nut it has some bugs.

---
+++ MySmoola.java
@@ -0,0 +1,21 @@
+import ast.VisitorImpl;
+import ast.VisitorType;
+import ast.node.Program;
+import org.antlr.v4.runtime.*;
+import java.io.IOException;
+
+public class MySmoola {
+    public static void main(String[] args) throws IOException {
+        CharStream reader = CharStreams.fromFileName(args[0]);
+        SmoolaLexer lexer = new SmoolaLexer(reader);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SmoolaParser parser = new SmoolaParser(tokens);
+        Program p = parser.program().p;
+        if (parser.program().error_count == 0) {
+        	System.out.println("alio");
+        	VisitorType v = new VisitorType(parser.program().synthesized_table);
+        	p.accept(v);	
+        }
+        // System.out.println("aliop");
+    }
+}
